f6c5646b483d31554515d75af731a32a4a08dee3
Akshay Tambe <akshay.design4art@gmail.com>
2022-10-06 21:11:30 +0530

    URL issue fixed

--- src/textUtils/components/NavBar.js
+++ src/textUtils/components/NavBar.js
@@ -8,7 +8,7 @@ export default function NavBar(props) {
      <nav className={`navbar navbar-expand-lg navbar-${props.modes} bg-${props.modes}`}>
      <div className="container-fluid">
        
-       <a className="navbar-brand" href="/">{props.title}</a>
+       <a className="navbar-brand" href="/react-text-utils">{props.title}</a>
        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
          <span className="navbar-toggler-icon"></span>
        </button>
